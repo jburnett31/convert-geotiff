9f8fb503a26d4bec68e17729ba4c61d66023c249
jburnett31 <jeffrey.burnett31@gmail.com>
2012-07-13 14:53:01 -0500

    working

--- src/get_geotiff_info/core.clj
+++ src/get_geotiff_info/core.clj
@@ -2,16 +2,16 @@
   (:gen-class :main true)
   (:require [geotiff.core :as geo]))
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (let [fpath (first args)
-        reader (geo/get-reader fpath)
+(defn run-this [arg]
+  (let [reader (geo/get-reader arg)
         metadata (geo/get-metadata reader)
         rootNode (geo/get-root-node metadata)
         [_ _ _ x1 y1 _] (geo/get-model-tie-points rootNode)
         [scale-x scale-y _] (geo/get-model-pixel-scales rootNode)
         [width height] (try (geo/get-dimensions reader) (catch Exception e))
         ]
-    (println x1 y1 scale-x scale-y width height)
     (println {:top y1 :left x1 :bottom (- y1 (* height scale-y)) :right (+ x1 (* width scale-x))})))
+
+(defn -main [& args]
+  (do
+    (run-this (first args))))
