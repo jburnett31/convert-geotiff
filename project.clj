efd208d3b07cca8199edaea3eb41fbd0013299c0
jburnett31 <jeffrey.burnett31@gmail.com>
2012-07-13 11:54:24 -0500

    added code

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-				 [geotiff "0.1.1"]])
+				 [geotiff "0.1.1"]]
+  :main get-geotiff-info.core)
--- src/get_geotiff_info/core.clj
+++ src/get_geotiff_info/core.clj
@@ -1,6 +1,16 @@
 (ns get-geotiff-info.core
   (:gen-class)
-  (:require [geotiff :as geo]))
+  (:require [geotiff :as geo])
+  (:import [java.io File]
+           [javax.imageio ImageReader ImageIO]))
+
+(defn get-dimensions [filepath]
+  (let [image-file (File. filepath)
+        iis (ImageIO/createImageInputStream file)
+        readers (ImageIO/getImageReadersByFormatName "tiff")
+        reader (.next readers)]
+    (.setInput reader iis)
+    [(.getWidth reader 0) (.getHeight reader 0)]))
 
 (defn -main
   "I don't do a whole lot."
@@ -8,5 +18,6 @@
   (let [metadata (geo/get-metadata (first args))
         rootNode (geo/get-root-node metadata)
         [_ _ _ x1 y1 _] (geo/get-model-tie-points rootNode)
-        [scale-x scale-y _] (geo/get-pixel-scales)]
-    ))
+        [scale-x scale-y _] (geo/get-pixel-scales)
+        [width height] (get-dimensions (first args))]
+    {:top y1 :left x1 :bottom (- y1 (* height scale-y)) :right (+ x1 (* width scale-x))}))
