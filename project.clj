ae1ea4ea6fb04ab44ca18b3b77e85c43b7d7453c
jburnett31 <jeffrey.burnett31@gmail.com>
2012-07-13 13:55:58 -0500

    refactored

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject get-geotiff-info "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject get-geotiff-info "0.1.0"
+  :description "Gets the extent of a geotiff image"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
--- src/get_geotiff_info/core.clj
+++ src/get_geotiff_info/core.clj
@@ -1,23 +1,17 @@
 (ns get-geotiff-info.core
   (:gen-class)
-  (:require [geotiff :as geo])
-  (:import [java.io File]
-           [javax.imageio ImageReader ImageIO]))
-
-(defn get-dimensions [filepath]
-  (let [image-file (File. filepath)
-        iis (ImageIO/createImageInputStream file)
-        readers (ImageIO/getImageReadersByFormatName "tiff")
-        reader (.next readers)]
-    (.setInput reader iis)
-    [(.getWidth reader 0) (.getHeight reader 0)]))
+  (:require [geotiff.core :as geo]))
 
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (let [metadata (geo/get-metadata (first args))
+  (let [fpath (first args)
+        reader (geo/get-reader fpath)
+        metadata (geo/get-metadata reader)
         rootNode (geo/get-root-node metadata)
         [_ _ _ x1 y1 _] (geo/get-model-tie-points rootNode)
-        [scale-x scale-y _] (geo/get-pixel-scales)
-        [width height] (get-dimensions (first args))]
-    {:top y1 :left x1 :bottom (- y1 (* height scale-y)) :right (+ x1 (* width scale-x))}))
+        [scale-x scale-y _] (geo/get-model-pixel-scales rootNode)
+        [width height] (try (geo/get-dimensions reader) (catch Exception e))
+        ]
+    (println x1 y1 scale-x scale-y width height)
+    (println {:top y1 :left x1 :bottom (- y1 (* height scale-y)) :right (+ x1 (* width scale-x))})))
