ac29fac2c509fc18f954e61926e1bf485ec7bc4d
jburnett31 <jeffrey.burnett31@gmail.com>
2012-07-18 15:31:43 -0500

    changed name

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject get-geotiff-info "0.1.0"
-  :description "Gets the extent of a geotiff image"
+(defproject convert-geotiff "0.1.0"
+  :description "Converts geotiff to png and stores extent info in a database"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -9,4 +9,4 @@
                  [clj-record "1.1.3"]
 			     [org.clojure/java.jdbc "0.2.3"]
 			     [mysql/mysql-connector-java "5.1.6"]]
-  :main get-geotiff-info.core)
+  :main convert-geotiff.core)
