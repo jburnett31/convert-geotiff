f7ae482a2c4d88e3498e8d4a6d48af908be97a2b
jburnett31 <jeffrey.burnett31@gmail.com>
2012-07-18 15:43:11 -0500

    deps

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-				 [geotiff "0.1.1"]
+				 [geotiff "0.1.1-alpha2"]
                  [im4clj "0.0.1-SNAPSHOT"]
                  [clj-record "1.1.3"]
 			     [org.clojure/java.jdbc "0.2.3"]
